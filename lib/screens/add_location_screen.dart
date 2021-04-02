7e933641d70c01d4a358c41572d185fef56cb721
Rares Cruceat <cruceat.rares@gmail.com>
2021-04-02 08:12:07 +0200

    init project

---
+++ lib/screens/add_location_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AddLocationScreen extends StatelessWidget {
+  static const String route = '/location/new';
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/location_details_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LocationDetailsScreen extends StatelessWidget {
+  static const route = '/location';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
