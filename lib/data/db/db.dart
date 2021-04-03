22e41a24738cdf0c73bc3a09f1f16aa303f1221c
Rares Cruceat <cruceat.rares@gmail.com>
2021-04-03 10:29:30 +0200

    add sqflite

---
+++ lib/data/db/db.dart
@@ -0,0 +1,10 @@
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:path/path.dart' as path;
+
+class DB {
+  static Future<void> insert(String table, Map<String, Object> data) async {
+    final dbPath = await sql.getDatabasesPath();
+    sql.openDatabase(path.join(dbPath, 'my_places.db'),
+        onCreate: (db, version) {});
+  }
+}
